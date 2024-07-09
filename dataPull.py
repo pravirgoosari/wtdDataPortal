85944ac9c6bae56111a5f15fcb4084ff9619da89
Amritha Prakash <amrithaprakash17@gmail.com>
2024-07-08 17:30:29 -0700

    Initial commit

--- dataPull.py
+++ dataPull.py
@@ -3,13 +3,16 @@ import pandas as pd
 import clr
 import datetime as dt
 import numpy as np
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 sys.path.append(r'C:\Program Files (x86)\PIPC\AF\PublicAssemblies\4.0')
 clr.AddReference('OSIsoft.AFSDK')
 
 from OSIsoft.AF.PI import PIServers, PIPoint
 from OSIsoft.AF.Data import AFBoundaryType
-from OSIsoft.AF.Time import AFTimeRange
+from OSIsoft.AF.Time import AFTimeRange, AFTimeSpan
 
 def connect_to_server(server_name):
     pi_servers = PIServers()
@@ -74,15 +77,53 @@ def retrieve_recorded_to_frame(tagname, pi_server, start, end, block_length=60):
     else:
         return None
 
+def retrieve_interpolated_to_frame(tagname, pi_server, start, end, interval, block_length=60):
+
+    point = PIPoint.FindPIPoint(pi_server, tagname)
+    af_time_span = AFTimeSpan.Parse(interval)
+    start_dt = pd.to_datetime(start)
+    end_dt = pd.to_datetime(end)
+    df_list = []
+    start1 = start_dt
+    end1 = start_dt + dt.timedelta(days=block_length)
+    if end1 > end_dt:
+        end1 = end_dt
+    while end1 < end_dt + dt.timedelta(days=block_length):
+        print('Retrieve {0} to {1}'.format(start1, end1))
+        af_time_range = AFTimeRange(start1.strftime('%Y-%m-%d %H:%M:%S'),
+                                    end1.strftime('%Y-%m-%d %H:%M:%S'))
+        interpolated = point.InterpolatedValues(af_time_range, af_time_span, '',
+                                                False)
+        data = convert_values_to_frame(interpolated, tagname)
+        if data is None:
+            print('No interpolated values for {0} between {1} and {2}'.format(
+                tagname, start1, end1))
+        else:
+            df_list.append(data)
+        start1 = end1 + pd.Timedelta(interval)
+        if (end1 + dt.timedelta(days=60) > end_dt) & (start1 < end_dt):
+            end1 = end_dt
+        else:
+            end1 = end1 + dt.timedelta(days=block_length)
+    data = pd.concat(df_list, axis=0)
+    if data is None:
+        print('No interpolated values for {0} between {1} and {2}'.format(
+            tagname, start, end))
+    else:
+        return data
+
 if __name__ == "__main__":
-    server_name = "SERVERNAME"
-    tagname = "TAGNAME"
+    server_name = os.getenv('SERVERNAME')  
+    tagname = os.getenv('pumpStationpump1') # PI tag name (string) 
     pi_server = connect_to_server(server_name)
     
-    start_time = "XXXX-XX-XX XX:XX:XX"
-    end_time = "XXXX-XX-XX XX:XX:XX"
+    # Input from user
+    start_time =  "XXXX-XX-XX XX:XX:XX" # "2024-07-08 17:19:00"
+    end_time = "XXXX-XX-XX XX:XX:XX" # "2024-07-08 08:19:00"
+    interval = "1m" # sample interval(string: examples: 1w=1 week, 1d=1 day, 1h=1 hour, 1m=1 minute, 1s=1 second)
     
-    data = retrieve_recorded_to_frame(tagname, pi_server, start_time, end_time)
+    #retrieve_recorded_to_frame(tagname, pi_server, start_time, end_time)
+    data = retrieve_interpolated_to_frame(tagname, pi_server, start_time, end_time, interval)
     if data is not None:
         print(data)
     else:
